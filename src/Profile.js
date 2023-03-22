2fe2e07ae8a66c7ed07ee08ffaec793605eeebd1
Ryan Bagan <darcknight1980@gmail.com>
2023-03-22 08:10:42 -0700

    added animation

--- src/Profile.js
+++ src/Profile.js
@@ -24,9 +24,8 @@ const Profile = () => {
     </div>
 
     <div className="right-side">
-
-    <h2 >TuneTastic
-    </h2>
+    <h2>TuneTastic</h2>
+    <h2>TuneTastic</h2>
     <p>With TuneTastic, you can easily browse through your music library and select your favorite songs, which can then be added to custom playlists that you create. </p>
     <LoginButton/>
     </div>
